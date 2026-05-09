d6d9f7f6a45bfd3bdfc4a91f06f2e07d9920852a
AkshitNest <sharmaakshit495@gmail.com>
2026-05-09 15:11:21 +0530

    Cleanup scratch files and refine Codeforces regex fallback

--- scratch/debug-auth0-pkg.js
+++
@@ -1,2 +0,0 @@
-import * as auth0Pkg from "@auth0/nextjs-auth0";
-console.log("Auth0 Package Exports:", Object.keys(auth0Pkg));
--- scratch/debug-auth0.js
+++
@@ -1,4 +0,0 @@
-import { auth0 } from "../lib/auth0.js";
-console.log("Auth0 instance:", auth0);
-console.log("Auth0 prototype:", Object.getPrototypeOf(auth0));
-console.log("Auth0 methods:", Object.getOwnPropertyNames(Object.getPrototypeOf(auth0)));
